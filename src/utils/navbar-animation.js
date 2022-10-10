f7bf70cf75c8209d47228665b254392eba16e6b9
ITurres <emanueliturres.co@gmail.com>
2022-10-10 12:47:25 -0300

    Add close navbar function

--- src/utils/navbar-animation.js
+++ src/utils/navbar-animation.js
@@ -1,10 +1,20 @@
 const $open_btn = document.querySelector("#open-top-navbar");
+const $close_btn = document.querySelector("#close-top-navbar");
 
 $open_btn.addEventListener("click", () => {
   open_NavBar();
 });
 
+$close_btn.addEventListener("click", () => {
+  close_NavBar();
+});
+
 const open_NavBar = () => {
   document.querySelector(".top-navbar").style.height = "3rem";
   document.querySelector(".top-navbar").style.top = "0";
+};
+
+const close_NavBar = () => {
+  document.querySelector(".top-navbar").style.height = "0";
+  document.querySelector(".top-navbar").style.top = "-100px";
 };

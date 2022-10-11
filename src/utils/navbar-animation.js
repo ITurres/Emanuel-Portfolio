79ca0cf65a2fa82f714b0d36c88b6e267fa0e7d5
ITurres <emanueliturres.co@gmail.com>
2022-10-10 22:02:34 -0300

    Refactor code

--- src/utils/navbar-animation.js
+++ src/utils/navbar-animation.js
@@ -1,20 +1,17 @@
 const $open_btn = document.querySelector("#open-navbar");
 const $close_btn = document.querySelector("#close-navbar");
+const $navBarElement = document.querySelector(".navbar");
 
-$open_btn.addEventListener("click", () => {
-  open_NavBar();
-});
+$open_btn.addEventListener("click", () => open_NavBar());
 
-$close_btn.addEventListener("click", () => {
-  close_NavBar();
-});
+$close_btn.addEventListener("click", () => close_NavBar());
 
 const open_NavBar = () => {
-  document.querySelector(".navbar").style.height = "3rem";
-  document.querySelector(".navbar").style.top = "0";
+  $navBarElement.style.height = "3rem";
+  $navBarElement.style.top = "0";
 };
 
 const close_NavBar = () => {
-  document.querySelector(".navbar").style.height = "0";
-  document.querySelector(".navbar").style.top = "-100px";
+  $navBarElement.style.height = "0";
+  $navBarElement.style.top = "-100px";
 };
